c83b5870d9519160ce09487142a877e11689f814
Hargun Kaur <hargunkaur286@gmail.com>
2024-11-21 00:24:05 +0530

    added a single file using multer

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const multer = require("multer");
+
+const app = express();
+
+//handling the uploading of a single file at first
+const upload = multer({dest: "uploads/"});
+
+app.post("/upload", upload.single("file"), (req, res) => {
+    res.json({status: "success"});
+});
+
+app.listen(4000, () => console.log("listening on port 4000"));
+
