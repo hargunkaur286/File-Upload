8e756ade6d3c96e9488922ec4d25f3e7d5f4423b
Hargun Kaur <hargunkaur286@gmail.com>
2024-11-21 01:02:18 +0530

    adding multiple files

--- index.js
+++ index.js
@@ -4,9 +4,17 @@ const multer = require("multer");
 const app = express();
 
 //handling the uploading of a single file at first
+
+// const upload = multer({dest: "uploads/"});
+
+// app.post("/upload", upload.single("file"), (req, res) => {
+//     res.json({status: "success"});
+// });
+
+//for multiple file uploads
 const upload = multer({dest: "uploads/"});
 
-app.post("/upload", upload.single("file"), (req, res) => {
+app.post("/upload", upload.array("file", 2), (req, res) => {
     res.json({status: "success"});
 });
 
