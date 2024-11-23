a7d51463bbed08cb77b5a8b768396e088876b01b
Hargun Kaur <hargunkaur286@gmail.com>
2024-11-23 16:55:30 +0530

    error handling

--- index.js
+++ index.js
@@ -43,16 +43,40 @@ const fileFilter = (req, file, cb) => {
   if (file.mimetype.split("/")[0] === "image") {
     cb(null, true);
   } else {
-    cb(new Error("File is not of the correct type!"));
+    cb(new multer.MulterError("LIMIT_UNEXPECTED_FILE"), false);
   }
 };
 
 // ["image", "jpeg"]
 
-const upload = multer({ storage, fileFilter, limits: { fileSize: 1000000000, files: 2 } });
+const upload = multer({
+  storage,
+  fileFilter,
+  limits: { fileSize: 1000000000, files: 2 },
+});
 
 app.post("/upload", upload.array("file"), (req, res) => {
   res.json({ status: "success" });
+});
+
+app.use((error, req, res, next) => {
+  if (error instanceof multer.MulterError) {
+    if (error.code == "LIMIT_FILE_SIZE") {
+      return resstatus(400).json({
+        message: "File is too large!",
+      });
+    }
+    if(error.code == "LIMIT_FILE_COUNT"){
+        return res.status(400).json({
+            message: "File limit reached!",
+        });
+    }
+    if(error.code == "LIMIT_UNEXPECTED_FILE"){
+        return res.status(400).json({
+            message: "File must be of image type!",
+        });
+    }
+  }
 });
 
 app.listen(4000, () => console.log("listening on port 4000"));
